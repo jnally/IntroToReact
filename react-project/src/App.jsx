be7313edb405f5ee0ec5026be93b149d2039168a
Jeremy Nally <jnally3@ivytech.edu>
2025-10-06 00:00:29 -0400

    more updates - jn

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useReducer} from "react";
 import "./App.css";
 import chef from "./images/chef.jpg";
 
@@ -23,11 +23,17 @@ const dishObjects = items.map((dish, i) => ({
   title: dish
 }));
 
-function Main( {dishes} ) {
+function Main( {dishes, openStatus, onStatus} ) {
   return (
     <>
       <div>
-        <h2>Welcome to this beautiful restaurant!</h2>
+        <button onClick={() => onStatus(true)}>
+          I want to be open
+        </button>
+        <h2>
+          Welcome to this beautiful restaurant!{" "}
+          {openStatus ? "Open" : "Closed"}
+        </h2>
       </div>
       <main>
         <img
@@ -48,16 +54,31 @@ function Main( {dishes} ) {
 }
 
 function App() {
-  const [status, setStatus] = useState("Open");
+  const [status, toggle] = useReducer(
+    (status) => !status,
+    true
+  );
 
+  useEffect(() => {
+    console.log(
+      `The restaurant is ${status ? "open" : "closed"}.`
+    );
+  }, [status]);
+  
   return (
     <div>
-      <h1>The restaurant is currently {status}.</h1>
-      <button onClick={() => setStatus("Closed")}>
-        Close Restaurant
+      <h1>The restaurant is currently {" "}
+        {status ? "open" : "closed"}.
+      </h1>
+      <button onClick={toggle}>
+        {status ? "Close" : "Open"} Restaurant
       </button>
       <Header name="Jeremy" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main 
+      dishes={dishObjects} 
+      openStatus={status} 
+      onStatus={toggle} 
+      />
     </div>
   );
 }
